5f5b30e18c8bdea74333c460ef4c38191f842761
Leo-Chan01 <ephraimleo16@gmail.com>
2021-09-30 09:39:58 +0100

    Done Deal

--- FormToAcceptUserDetails.java
+++ FormToAcceptUserDetails.java
@@ -1,9 +1,37 @@
 public class FormToAcceptUserDetails {
     public static void main(String[] args) {
+        System.out.println("The Following Form Requests you to input your details");
+        try {
+            Thread.sleep(3000);
+        } catch (Exception e) {
+            //TODO: handle exception
+        }
         
         UserDetails studentDetails = new UserDetails();
         studentDetails.getName();
         studentDetails.getRegNo();
         studentDetails.getDepartment();
+        try {
+            insertData();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Thank you For inputing your details");
+    }
+
+    public static void insertData() throws InterruptedException{
+        Thread.sleep(1000);
+        System.out.println("Initialising Data insertion...");
+        Thread.sleep(500);
+        System.out.println("Inserting Data.<15%>");
+        Thread.sleep(500);
+        System.out.println("Inserting Data..<50%>");
+        Thread.sleep(200);
+        System.out.println("Inserting Data...<75%>");
+        Thread.sleep(3000);
+        System.out.println("Inserting Data...<100%>");
+        Thread.sleep(100);
+        System.out.println("Successfully Inserted");
+        Thread.sleep(500);
     }
 }
