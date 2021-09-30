937693f3f0e3c04a45685f020aa0abcba80a6b61
Leo-Chan01 <ephraimleo16@gmail.com>
2021-09-30 08:58:28 +0100

    Written Code to print shapes

--- ShapePrinter.java
+++ ShapePrinter.java
@@ -1,5 +1,9 @@
 public class ShapePrinter {
     public static void main(String[] args) {
-        
+        System.out.println("Square \n");
+        System.out.println("Circle \n");
+        System.out.println("Oval \n");
+        System.out.println("Rectangle \n");
+        System.out.println("Triangle \n");
     }
 }
